93543bef6290d892b18b2faa2006944771d08cfb
Billy Noyes <wbillypn@gmail.com>
2021-06-27 13:39:21 +0100

    added fingerprint icon to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { MdFingerprint } from 'react-icons/md';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
         <>
             <div className='navbar'>
                 <div className='navbar-container container'>
-                    <Link className='navbar-logo' to='/'>LAVISH</Link>
+                    <Link className='navbar-logo' to='/'>
+                        <MdFingerprint className='navbar-icon' />
+                        LAVISH
+                    </Link>
                     <div className='menu-icon'>
+
                     </div>
                 </div>
             </div>

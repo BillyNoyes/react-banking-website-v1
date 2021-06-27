974e626724b81de5ff2f8d237a6ea1af4789a562
Billy Noyes <wbillypn@gmail.com>
2021-06-27 13:41:03 +0100

    added router, switch and route to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
   );
 }
 

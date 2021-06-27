5cc1b1b677b0c3d0b72defa860ed406bbd292693
Billy Noyes <wbillypn@gmail.com>
2021-06-27 16:09:51 +0100

    added code for the rest of the components required

--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import HeroSection from '../../HeroSection';
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
-// import Pricing from '../../Pricing';
+import Pricing from '../../Pricing';
 
-function Home() {
+function SignUp() {
   return (
     <>
       <HeroSection {...homeObjOne} />
       <HeroSection {...homeObjThree} />
-      <HeroSection {...homeObjTwo} />
-      {/* <Pricing /> */}
-      <HeroSection {...homeObjFour} />
     </>
   );
 }
 
-export default Home;
+export default SignUp;
